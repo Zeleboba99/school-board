05ff87b270bb3fa09418053b2d8563636cd560fb
kate <katee241199@gmail.com>
2020-03-31 19:37:14 +0300

    add repositories

---
+++ backend/src/main/java/ru/vsu/board/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package ru.vsu.board.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.vsu.board.model.Post;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
