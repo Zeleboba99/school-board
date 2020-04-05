f24156a545ea1138cd3c65b64acc0fee957e4132
Ilya Mesheryakov <mescheryakov.i.g@gmail.com>
2020-04-05 13:45:20 +0300

    Login to frontend added

--- frontend/board/src/app/app.module.ts
+++ frontend/board/src/app/app.module.ts
@@ -3,16 +3,25 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {LoginComponent} from './login/login.component';
+import {AppRoutingModule} from './app-routing.module';
+import {authInterceptorProviders} from './helpers/auth.interceptor';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [authInterceptorProviders],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
