f3d965b5a9c0974ff0c4a641b032c25d466a2f33
kate <katee241199@gmail.com>
2020-04-12 21:52:33 +0300

    fix swagger configuration

--- backend/src/main/java/ru/vsu/board/BoardApplication.java
+++ backend/src/main/java/ru/vsu/board/BoardApplication.java
@@ -4,27 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 @SpringBootApplication
-@EnableSwagger2
 public class BoardApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BoardApplication.class, args);
-    }
-
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("ru.vsu.board.controller"))
-                .paths(PathSelectors.any())
-                .build();
     }
 
 }
